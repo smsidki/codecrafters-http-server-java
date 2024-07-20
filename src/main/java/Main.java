cc97aaa7cee93de19b91529d642e96819e18ce1c
sayid.sidqi <sayid.sidqi@accenture.com>
2024-07-20 09:31:40 +0700

    write http response

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,9 @@
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Main {
+
   public static void main(String[] args) {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     System.out.println("Logs from your program will appear here!");
@@ -13,11 +15,20 @@ public class Main {
        // ensures that we don't run into 'Address already in use' errors
        serverSocket.setReuseAddress(true);
 
-       serverSocket.accept(); // Wait for connection from client.
+       var clientSocket = serverSocket.accept(); // Wait for connection from client.
        System.out.println("accepted new connection");
+
+       writeResponse(clientSocket, "", "");
      } catch (IOException e) {
        System.out.println("IOException: " + e.getMessage());
      }
+  }
+
+  static void writeResponse(Socket clientSocket, String header, String body) throws IOException {
+    try(var os = clientSocket.getOutputStream()) {
+      os.write("HTTP/1.1 200 OK\r\n%s\r\n%s".formatted(header, body).getBytes());
+      os.flush();
+    }
   }
 
 }
